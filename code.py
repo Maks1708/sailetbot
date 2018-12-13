e0f7848c6a85a8619b836289fe46c0dcd32dc7d3
Максим Беляков <belmax1708@gmail.com>
2018-12-14 02:17:46 +0600

    6

--- code.py
+++ code.py
@@ -28,31 +28,21 @@ def get_name(message):
     
     bot.send_message(message.chat.id,"Пожалуйста введите компанию которую вы представляете")
     bot.register_next_step_handler(message,get_company)
-    user_inf.append(message.text)
+    user_inf[0] = message.text
     
 def get_company(message):
     
     bot.send_message(message.chat.id,"Пожалуйста введите ваш номер телефона")
     print(message.chat.id)
     bot.register_next_step_handler(message,get_phone)
-    user_inf.append(message.text)
+    user_inf[1] = message.text
 
 def get_phone(message):
-    
-    bot.register_next_step_handler(message, send_inf_to_admin)
-    user_inf.append(message.text)
-
-def send_inf_to_admin(message):
-    print(user_inf)
+    user_inf[2] = message.text
     main_stroke = "Имя: " + user_inf[0] + " \nКомпания: " + user_inf[1] + " \nТелефон: " + user_inf[2]
     bot.send_message(410680216, main_stroke)
 
     bot.send_message(message.chat.id, "Спасибо ваше обращение успешно подано, если вы захотите отправить нам ещё одно, то воспользуйтесть командой /start")
-
-
-    
-    
-
 
 if __name__ == '__main__':
     bot.polling(none_stop = True)

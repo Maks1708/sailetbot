7683ce62832cf3b83470c62b5281f29ce7d35949
Максим Беляков <belmax1708@gmail.com>
2018-12-14 13:19:06 +0600

    8

--- code.py
+++ code.py
@@ -10,7 +10,7 @@ import time
 
 bot = telebot.TeleBot(config.token)
 
-user_inf = [0, 0, 0]
+user_inf = []
 
 @bot.message_handler(commands = ['start'])
 def start(message):
@@ -28,21 +28,32 @@ def get_name(message):
     
     bot.send_message(message.chat.id,"Пожалуйста введите компанию которую вы представляете")
     bot.register_next_step_handler(message,get_company)
-    user_inf[0] = message.text
+    user_inf.append(message.text)
     
 def get_company(message):
     
     bot.send_message(message.chat.id,"Пожалуйста введите ваш номер телефона")
     print(message.chat.id)
     bot.register_next_step_handler(message,get_phone)
-    user_inf[1] = message.text
+    user_inf.append(message.text)
 
 def get_phone(message):
-    user_inf[2] = message.text
+    
+    bot.send_message(message.chat.id,"Какова цель вашего обращения")
+    bot.register_next_step_handler(message, send_inf_to_admin)
+    user_inf.append(message.text)
+
+def send_inf_to_admin(message):
+    print(user_inf)
     main_stroke = "Имя: " + user_inf[0] + " \nКомпания: " + user_inf[1] + " \nТелефон: " + user_inf[2]
     bot.send_message(410680216, main_stroke)
 
     bot.send_message(message.chat.id, "Спасибо ваше обращение успешно подано, если вы захотите отправить нам ещё одно, то воспользуйтесть командой /start")
+
+
+    
+    
+
 
 if __name__ == '__main__':
     bot.polling(none_stop = True)
